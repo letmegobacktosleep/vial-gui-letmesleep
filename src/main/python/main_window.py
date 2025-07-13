850e627e2f623b27b66ba585c9d19e04263ca998
letmegobacktosleep <winstonkwan06@gmail.com>
2025-07-13 17:41:52 +1000

    idk

--- src/main/python/main_window.py
+++ src/main/python/main_window.py
@@ -86,9 +86,9 @@ class MainWindow(QMainWindow):
         self.matrix_tester = MatrixTest(self.layout_editor)
         self.rgb_configurator = RGBConfigurator()
 
-        self.editors = [(self.keymap_editor, "Keymap"), (self.halleffect_editor, "Hall Effect"), (self.layout_editor, "Layout"),
-                        (self.macro_recorder, "Macros"), (self.rgb_configurator, "Lighting"), (self.tap_dance, "Tap Dance"),
-                        (self.combos, "Combos"), (self.key_override, "Key Overrides"), (self.qmk_settings, "QMK Settings"),
+        self.editors = [(self.keymap_editor, "Keymap"), (self.halleffect_editor, "Hall Effect"), (self.layout_editor, "Layout"), (self.macro_recorder, "Macros"),
+                        (self.rgb_configurator, "Lighting"), (self.tap_dance, "Tap Dance"), (self.combos, "Combos"),
+                        (self.key_override, "Key Overrides"), (self.qmk_settings, "QMK Settings"),
                         (self.matrix_tester, "Matrix tester"), (self.firmware_flasher, "Firmware updater")]
 
         Unlocker.global_layout_editor = self.layout_editor
@@ -313,8 +313,8 @@ class MainWindow(QMainWindow):
             Unlocker.unlock(self.autorefresh.current_device.keyboard)
             self.autorefresh.current_device.keyboard.reload()
 
-        for e in [self.layout_editor, self.keymap_editor, self.halleffect_editor, self.firmware_flasher,
-                  self.macro_recorder, self.tap_dance, self.combos, self.key_override, self.qmk_settings,
+        for e in [self.layout_editor, self.keymap_editor, self.halleffect_editor, self.firmware_flasher, self.macro_recorder, 
+                  self.tap_dance, self.combos, self.key_override, self.qmk_settings,
                   self.matrix_tester, self.rgb_configurator]:
             e.rebuild(self.autorefresh.current_device)
 
